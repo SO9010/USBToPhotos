c23deb9975de90833bf54e71bc39d83f3b839bcc
SO9010 <so9010sami@gmail.com>
2022-08-20 23:30:51 +0100

    update

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
-import os
-
+from os import walk
+from os import listdir
 #settings
 
 move = False
 copy = True
 
 goDir = "~/Pictures/"		#change this if you want the photos to go somewhere else :)
-fromDir = "/run/media" + os.getlogin()
+fromDir = "/run/media/samio"
 
-for files in os.walk("fromDir"):
-	print(files)
+imageEnds = ["avif", "gif", "jpg", "jpeg", "jfif", "pjpeg", "pjp", "png", "svg", "webp"]
+
+blackList = ["HardDrive"]
+
+for i in listdir(fromDir): 
+    if i not in blackList:
+        for files in walk(fromDir + "/" + i):
+            if len(files[2]) != 0:
+                print(files[2])
+                for p in files[2]:
+                    if p.endswith(tuple("xml")):
+                        print(p)
